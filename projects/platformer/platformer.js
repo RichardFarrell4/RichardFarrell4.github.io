15c17425339a4eff5ab6821523c64fa6f210019f
RichardFarrell4 Farrell <richard.farrell@sciacademy.org>
2023-11-02 16:13:22 +0000

    add bouncing box

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,12 +44,10 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
    
-    createPlatform(250, 700, 300, 20);
-    createPlatform(300, 500, 400, 20);
-    createPlatform(250, 350, 200, 10);
-    createPlatform(750, 1050, 300, 20);
-    createPlatform(900, 650, 200, 20);
-   createPlatform(800, 300, 400, 20);
+    createPlatform(200, 700, 500, 20);
+    createPlatform(110, 800, 400, 30);
+    createPlatform(400, 600, 300, 25);
+    createPlatform(300, 850, 600, 20);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
@@ -65,9 +63,10 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("right", 600, 1000, 10, 10);
-    createCannon("top", 700, 1500, 20, 10);
+    createCannon("right", 250, 400, 10, 10);
+    createCannon("top", 400, 1000, 20, 10);
     createCannon("left", 400, 790, 30, 20); 
+    createCannon("right", 500, 300, 60, 30);
 
 
     /////////////////////////////////////////////////
